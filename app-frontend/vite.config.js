d40a0f93d460a248585fc997b386452fba4236b4
Deep senchowa <62834469+deep663@users.noreply.github.com>
2025-02-06 19:48:53 +0530

    Update vite.config.js

--- app-frontend/vite.config.js
+++ app-frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'http://localhost:8000' ,
+      '/api': 'https://exercise-app-m5xi.onrender.com' ,
     },
   },
   plugins: [react()],
